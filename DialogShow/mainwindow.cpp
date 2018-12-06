947808d91e7871ec2f0dce0cf90b235a0ae1073c
Terence Chan Zun Mun <zunmun@gmail.com>
2018-12-06 22:33:16 +0800

    DialogShow
    centre the button

--- DialogShow/mainwindow.cpp
+++ DialogShow/mainwindow.cpp
@@ -2,17 +2,21 @@
 
 #include <QCoreApplication>
 #include "dialog.h"
+#include <QtWidgets>
 
 MainWindow::MainWindow(QWidget *parent)
    : QMainWindow(parent)
 {
    // Create the button, make "this" the parent
    button = new QPushButton("Show Dialog", this);
-   // set size and location of the button
-   //m_button->setGeometry(QRect(QPoint(100, 100),QSize(200, 50)));
-
-
    connect(button, SIGNAL (released()), this, SLOT (handleButton()));
+
+   QGridLayout *mainLayout = new QGridLayout;
+   mainLayout->addWidget(button);
+
+   QWidget *widget = new QWidget();
+   widget->setLayout(mainLayout);
+   setCentralWidget(widget);
 }
 
 void MainWindow::handleButton()
